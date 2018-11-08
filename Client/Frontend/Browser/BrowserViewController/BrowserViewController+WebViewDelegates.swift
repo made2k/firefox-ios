f28b57f089e5fd397e42743fd952a9a76f0ebecd
Zach McGaughey <1799888+made2k@users.noreply.github.com>
2018-11-08 07:28:05 -0700

    Added adguard adblocking

--- Client/Frontend/Browser/BrowserViewController/BrowserViewController+WebViewDelegates.swift
+++ Client/Frontend/Browser/BrowserViewController/BrowserViewController+WebViewDelegates.swift
@@ -485,29 +485,3 @@ private class PreviewingWebViewController: UIViewController {
         }
     }
 }
-
-
-private class PreviewingWebViewController: UIViewController {
-
-    private let webView: WKWebView
-    let request: URLRequest
-
-    init(request: URLRequest, configuration: WKWebViewConfiguration) {
-        self.request = request
-        webView = WKWebView(frame: .zero, configuration: configuration)
-        webView.load(request)
-        super.init(nibName: nil, bundle: nil)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.addSubview(webView)
-        webView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-    }
-}
